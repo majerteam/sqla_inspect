b5a5bc57c74b01fcb9a8ea6e72439dc4f9c7f1d7
G.Tjebbes <g.t@majerti.fr>
2015-02-05 00:46:08 +0100

    Update the version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='sqla_inspect',
-    version='0.1',
+    version='0.1.1',
     packages=['sqla_inspect'],
     include_package_data=True,
     license='GPLv3',
