61fc08a9f0f7ff5465d60d13c3547199701c9b1d
G.Tjebbes <g.t@majerti.fr>
2015-09-22 16:56:26 +0200

    Update version to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='sqla_inspect',
-    version='0.1.9',
+    version='0.2.0',
     packages=['sqla_inspect'],
     include_package_data=True,
     license='GPLv3',
