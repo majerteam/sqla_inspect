3762251306a0e7516764759a0314848450c62948
G.Tjebbes <g.t@majerti.fr>
2022-04-21 16:16:26 +0200

    0.7.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='sqla_inspect',
-    version='0.7.1',
+    version='0.7.2',
     packages=['sqla_inspect'],
     include_package_data=True,
     license='GPLv3',
